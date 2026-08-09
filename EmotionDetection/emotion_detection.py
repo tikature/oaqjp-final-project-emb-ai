9bc0b9bff6636c956619b8dcc726972ed725d3c2
tikature <detikaa10@gmail.com>
2026-08-09 20:23:36 +0700

    Add emotion detection functionality with emotion_detector

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,36 @@
+import requests
+
+URL = (
+    "https://sn-watson-emotion.labs.skills.network/"
+    "v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+)
+
+HEADERS = {
+    "grpc-metadata-mm-model-id":
+        "emotion_aggregated-workflow_lang_en_stock"
+}
+
+
+def emotion_detector(text_to_analyze):
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(
+        URL,
+        json=input_json,
+        headers=HEADERS
+    )
+
+    emotions = response.json()["emotionPredictions"][0]["emotion"]
+
+    dominant_emotion = max(
+        emotions,
+        key=emotions.get
+    )
+
+    emotions["dominant_emotion"] = dominant_emotion
+
+    return emotions
